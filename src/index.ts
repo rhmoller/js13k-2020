4e1cff3c9536f4dbe7e537a3539aed73c64db4b0
Rene Hangstrup Møller <rhmoller@gmail.com>
2020-08-12 23:15:07 +0200

    chore: update three and include VRButton

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { VRButton } from "./VRButton"; // this seems to be missing in https://js13kgames.com/webxr-src/2020/three.js
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -6,3 +7,17 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+document.body.appendChild(VRButton.createButton(renderer));
+renderer.xr.enabled = true;
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+cube.position.z = -5;
+scene.add(cube);
+
+renderer.setAnimationLoop(() => {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+});
